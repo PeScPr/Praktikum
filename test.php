f1983d4f3ae13a80a9d16ff51f93584cda0e1722
PeScPr <36845777+PeScPr@users.noreply.github.com>
2018-03-28 12:24:44 +0200

    Test Commit

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+
+	$farbe = "blue";
+	$bgfarbe = "#FFF";
+	
+	$msg="<table border='0' style='font-size: 10pt; color: $farbe;'>";
+?>
+		<script language="javascript">
+				alert ("Hallo");
+		</script>
+<?php
+	$msg=$msg . "</table>";
+	echo $msg;
+?>
